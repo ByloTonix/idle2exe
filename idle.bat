af196d4c88e62ee41ea3773d401f2e41bdf1e969
MatroCholo <arkady260705@gmail.com>
2021-06-07 16:53:36 +0300

    v3.9.0.1

--- idle.bat
+++ idle.bat
@@ -1,4 +1,3 @@
 @echo off
 rem Start IDLE using the appropriate Python interpreter
-set CURRDIR=%~dp0
 start "IDLE" "C:\Program Files\Python39\pythonw.exe" "C:\Program Files\Python39\Lib\idlelib\idle.pyw" %1 %2 %3 %4 %5 %6 %7 %8 %9
