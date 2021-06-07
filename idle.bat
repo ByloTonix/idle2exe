62ac59de59da86c1ab35fb8363f1dae988b68cf5
MatroCholo <arkady260705@gmail.com>
2021-06-07 16:51:39 +0300

    v3.9.0.1

---
+++ idle.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGmW+0UiKRYUag2OOXiGNqwI4Of1vLO7jkQfaOs7dIrX07ayCeMc5FbYfJIh0zRTm8Rs
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJjZkIaHErTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsHAl3MbCXrZg==
+::ZQ05rAF9IAHYFVzEqQIZDDdxDEyPM2/6BKxcxenv/eWkqkIRW6tf
+::eg0/rx1wNQPfEVWB+kM9LVsJDC2mEE7a
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQITPQhAQwnCFU+WI/UV5qj+9+/HpEQRUYI=
+::dhA7uBVwLU+EWHSF4FA/CxNbQAvi
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDZVWBaNH2O1CrogzO3o5P6IsnE0cM4aNorD39Q=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+rem Start IDLE using the appropriate Python interpreter
+set CURRDIR=%~dp0
+start "IDLE" "C:\Program Files\Python39\pythonw.exe" "C:\Program Files\Python39\Lib\idlelib\idle.pyw" %1 %2 %3 %4 %5 %6 %7 %8 %9
