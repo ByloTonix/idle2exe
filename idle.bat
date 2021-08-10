ea7fc3c96fa131338af38435573872be46539cd2
MatroCholo <64357478+MatroCholo@users.noreply.github.com>
2021-08-10 21:57:09 +0300

    Delete idle.bat

--- idle.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem Start IDLE using the appropriate Python interpreter
-start "IDLE" "C:\Program Files\Python39\pythonw.exe" "C:\Program Files\Python39\Lib\idlelib\idle.pyw" %1 %2 %3 %4 %5 %6 %7 %8 %9
